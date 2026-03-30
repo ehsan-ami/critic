be277577aca6410795f0c705173afb0c208fd10f
Ehsan Ahmadi <54815964+ehsan-ami@users.noreply.github.com>
2026-03-30 19:36:22 -0400

    Fix navbar: add mobile burger menu and rel attributes

--- static/js/index.js
+++ static/js/index.js
@@ -3,6 +3,10 @@ window.HELP_IMPROVE_VIDEOJS = false;
 
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
 
     var options = {
 			slidesToScroll: 1,
